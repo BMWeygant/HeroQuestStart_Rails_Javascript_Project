30b00b8412f4bc16dd58903a4971c9410bf77805
Brandon Weygant <bobbydigital108@gmail.com>
2019-06-04 12:42:37 -0400

    Updated config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#home'
+  resources :heros
+  resources :adventures
+  resources , only: [:new, :create, :edit, :destroy]
+  post '/experiences', to: 'experiences#create'
 end
