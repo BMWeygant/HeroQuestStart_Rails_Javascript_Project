5f5b9d081078504b7d6af58725ca23e4d130d211
Brandon Weygant <bobbydigital108@gmail.com>
2019-06-04 13:33:56 -0400

    Added Devise User Controllers/Views

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class Users::SessionsController < Devise::SessionsController
+  # before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  def new
+  end
+
+  # POST /resource/sign_in
+  def create
+    user = User.find_by(:name => params[:user][:name])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      render :new
+    end
+  end
+
+  # DELETE /resource/sign_out
+  def destroy
+    if current_user
+      session.delete :user_id
+      redirect_to root_url
+    end
+   end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+end
