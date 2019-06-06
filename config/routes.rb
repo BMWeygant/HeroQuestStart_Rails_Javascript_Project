b4c49b9df1b621e86b6461e8ca0b22f3e339b5fc
BMWeygant <bobbydigital108@gmail.com>
2019-06-05 23:57:17 -0400

    Added nested resource heros/new to users

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#home'
   resources :users do
-    resources :heros, only: [:index]
+    resources :heros, only: [:index, :new]
   end
-  resources :heros, except: [:index]
+  resources :heros, except: [:index, :new]
   resources :adventures
   resources :experiences
   #post '/experiences', to: 'experiences#create'
