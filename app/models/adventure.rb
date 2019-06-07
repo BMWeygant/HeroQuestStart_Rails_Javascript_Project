2ecc94f8c95a687948c9c892edf52c36075fe02f
Brandon Weygant <bobbydigital108@gmail.com>
2019-06-06 22:22:07 -0400

    Got the little sister boolean to update through checkbox in experiences/show and added logic to adventure model to update hero accordingly.

--- app/models/adventure.rb
+++ app/models/adventure.rb
@@ -6,11 +6,16 @@ belongs_to :user
 
   def set_out_on_adventure
   #  binding.pry
-  
-    self.hero.update(:hp => (self.hero.hp - self.experience.hp_rating),
+    if self.little_sister == false
+      self.hero.update(:hp => (self.hero.hp - self.experience.hp_rating),
                          :treasure => (self.hero.treasure + self.experience.treasure_rating),
                          :total_xp => (self.hero.total_xp + self.experience.xp)
                         )
-
-      end
+    else
+      self.hero.update(:hp => (self.hero.hp - self.experience.hp_rating + 2),
+                         :treasure => (self.hero.treasure + self.experience.treasure_rating),
+                         :total_xp => (self.hero.total_xp + self.experience.xp + 5)
+                        )
     end
+  end
+end
