99011319997064be3b62a6b8c3418fda67f33f59
Brandon Weygant <bobbydigital108@gmail.com>
2019-07-08 12:45:54 -0400

    Built constructor function in heros.js

--- app/assets/javascripts/heros.js
+++ app/assets/javascripts/heros.js
@@ -7,6 +7,22 @@ const bindClickHandlers = () => {
     e.preventDefault()
     fetch(`/users/` + $(this).id + `/heros.json`)
       .then(res => res.json())
-      .then(data => console.log(data))
+      .then(heros => {
+      $('#app-container').html('')
+      heros.forEach(hero => {
+        let newHero = new Hero(hero)
+        console.log(newHero)
+      })
+    })
   })
+}
+
+function Hero(hero) {
+  this.id = hero.id
+  this.name = hero.name
+  this.hp = hero.hp
+  this.treasure = hero.treasure
+  this.total_xp = hero.total_xp
+  this.user = hero.user
+  this.incapacitated = hero.incapacitated
 }
