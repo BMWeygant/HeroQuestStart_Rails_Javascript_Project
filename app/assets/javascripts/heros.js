ac8074b1c4cdf847c27c1dff6d10cfed678287e9
Brandon Weygant <bobbydigital108@gmail.com>
2019-07-08 12:16:51 -0400

    Added navigation partial and uncluttered application.html.erb file.

--- app/assets/javascripts/heros.js
+++ app/assets/javascripts/heros.js
@@ -6,16 +6,7 @@ const bindClickHandlers = () => {
   $('.all_heros').on('click', (e) => {
     e.preventDefault()
     fetch(`/users/` + $(this).id + `/heros.json`)
-      .then((res) => res.json())
+      .then(res => res.json())
       .then(data => console.log(data))
   })
 }
-
-// const bindClickHandlers = () => {
-//   $('.all_heros').on('click', (e) => {
-//     e.preventDefault()
-//     fetch(`/users/:user_id/heros.json`)
-//       .then(res => res.json())
-//       .then(data => console.log(data))
-//   })
-// }
