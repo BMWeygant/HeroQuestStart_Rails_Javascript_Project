e3f8b4f657cc97e5c0f3d7c2d8e41350b771bb9d
Brandon Weygant <bobbydigital108@gmail.com>
2019-07-11 14:25:10 -0400

    Succesfully added functional incapacitated logic to adventure model and controller, specifically the inability to quest if hero hp =< 0 & sets hp to 0. Improved healing mechanics.

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -3,11 +3,16 @@ before_action :authenticate_user!
   def create
     @adventure = Adventure.create(adventure_params)
     @adventure.user_id = current_user.id
-    #binding.pry
 
     if @adventure
       @adventure.set_out_on_adventure
+      if @adventure.hero.incapacitated == true && @adventure.experience.healing == false
+        flash[:word] = "#{@adventure.hero.name} is incapacitated and can not go on adventures at this time."
+      elsif @adventure.experience.hp_rating == 0
+        flash[:word] = "#{@adventure.hero.name} can not afford healing. Do more quests to collect more treasure!"
+      else
         flash[:word] = "Your hero has returned!"
+      end
       redirect_to hero_path(@adventure.hero)
     else
       redirect_to experience_path
--- app/models/adventure.rb
+++ app/models/adventure.rb
@@ -7,19 +7,24 @@ belongs_to :user
   def set_out_on_adventure
   #  binding.pry
   clean_code
-    if little_sister == false
+    if hero.incapacitated == true
+        hero.update(:hp => (hero.hp*0),
+                    :treasure => (hero.treasure*1),
+                    :total_xp => (hero.total_xp*1)
+                  )
+    elsif little_sister == false
+      healing
       hero.update(:hp => (hero.hp - experience.hp_rating),
                          :treasure => (hero.treasure + experience.treasure_rating),
                          :total_xp => (hero.total_xp + experience.xp)
                         )
-                        healing
                         incapacitated
     else
-      hero.update(:hp => (hero.hp - experience.hp_rating + 2),
-                         :treasure => (hero.treasure + experience.treasure_rating),
-                         :total_xp => (hero.total_xp + experience.xp + 5)
+      healing
+      hero.update(:hp => (hero.hp - experience.hp_rating + - rand(1..15)),
+                         :treasure => (hero.treasure + experience.treasure_rating + rand(1..20) - 10),
+                         :total_xp => (hero.total_xp + experience.xp + 5 * rand(0..3))
                         )
-                        healing
                         incapacitated
     end
   end
@@ -27,16 +32,28 @@ belongs_to :user
   def incapacitated
     clean_code
     if hero.hp <= 0
-        hero.update(:incapacitated => true)
+        hero.update(:hp => (hero.hp*0),
+                    :incapacitated => true)
+    else
+      hero.update(:incapacitated => false)
     end
   end
 
   def healing
     clean_code
-    if experience.healing == true
+    if experience.healing == false
+      nil
+    elsif experience.healing == true && hero.treasure >= 20
+      experience.update(:treasure_rating => (experience.treasure_rating*0),
+                        :xp => (experience.xp*0))
       hero.update(:hp => (hero.hp + 10),
-                              :treasure => (hero.treasure - 4)
+                              :treasure => (hero.treasure - 20)
                             )
+    else
+      experience.update(:hp_rating => (experience.hp_rating*0),
+                        :treasure_rating => (experience.treasure_rating*0),
+                        :xp => (experience.xp*0)
+                      )
                           end
   end
 
