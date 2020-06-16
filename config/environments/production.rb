60d42d3af881bd7ca25b1f60ef485cf7d33be3c1
BMWeygant <bobbydigital108@gmail.com>
2020-06-16 12:25:00 -0400

    Update gems and added actionmailer language to development & production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,18 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["EMAIL_USERNAME"],
+    password:ENV["EMAIL_PASSWORD"]
+  }
+# doesn't have to be Heroku, but you get the idea.
+config.action_mailer.default_url_options = { :host => 'https://heroqueststart.herokuapp.com/' }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
